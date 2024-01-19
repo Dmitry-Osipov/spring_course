63225f2d3f0b7d9b871ac37816c3e64b742177ed
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-19 15:15:55 +0300

    Studied the performance of the Autowired annotation on setters

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -9,17 +9,17 @@ public class Person {
     private String surname;
     private int age;
 
-//    public Person() {
-//        System.out.println("Использовался конструктор без аргументов");
-//    }
-
-    @Autowired  // Автоматическое внедрение зависимостей благодаря аннотации Autowired. Начиная с версии Spring 7, при
-    // наличии у класса одного конструктора, можно не указывать аннотацию Autowired, ибо Spring будет внедрять
-    // зависимость на одном этом конструкторе.
-    public Person(Pet pet) {
-        System.out.println("Использовался конструктор с аргументом");
-        this.pet = pet;
+    public Person() {
+        System.out.println("Использовался конструктор без аргументов");
     }
+
+//    @Autowired  // Автоматическое внедрение зависимостей благодаря аннотации Autowired. Начиная с версии Spring 7, при
+//    // наличии у класса одного конструктора, можно не указывать аннотацию Autowired, ибо Spring будет внедрять
+//    // зависимость на одном этом конструкторе.
+//    public Person(Pet pet) {
+//        System.out.println("Использовался конструктор с аргументом");
+//        this.pet = pet;
+//    }
 
     public String getSurname() {
         return surname;
@@ -39,6 +39,8 @@ public class Person {
         this.age = age;
     }
 
+    @Autowired  // Необязательно эту аннотацию подставлять именно к сеттеру. По факту её можно использовать с любым
+    // методом.
     public void setPet(Pet pet) {
         System.out.println("Использовался сеттер животного");
         this.pet = pet;
