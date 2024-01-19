4ee48c86811790e843c503ec289e0b87c2016f2f
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-19 14:23:50 +0300

    Studied the init and destroy methods under different scopes

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,22 +1,23 @@
 package spring_introduction;
 
 public class Dog implements Pet {
-    private String name;
-
     public Dog() {
         System.out.println("Создание собаки через конструктор");
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
     public void say() {
         System.out.println("Bow-Wow");
+    }
+
+    public void init() {  // Метод инициализации срабатывает сразу после создания класса. Модификатор доступа, как и
+        // тип возвращаемых значений, может быть любым. Параметров быть не должно.
+        System.out.println("Init method класса собаки");
+    }
+
+    public void destroy() {  // Метод уничтожения срабатывает перед завершением приложения (закрытием контекста). Однако
+        // если Bean Scope = prototype, то метод инициализации будет вызываться для каждого объекта, а метод уничтожения
+        // вызываться не будет. Программисту необходимо самостоятельно писать код для закрытия/освобождения ресурсов.
+        System.out.println("Destroy method класса собаки");
     }
 }
