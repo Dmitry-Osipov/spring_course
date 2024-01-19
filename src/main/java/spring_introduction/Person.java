a3d05499b16669122a040cdf7d0e7969a491ca39
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-19 16:54:19 +0300

    Studied a Qualifier annotation that helps avoid DI issues when using more than one Bean

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,12 +1,16 @@
 package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
-    @Autowired  // Под капотом Spring внедрит зависимость, используя рефлексию. Данный вариант не является
-    // рекомендуемым.
+//    @Autowired  // Под капотом Spring внедрит зависимость, используя рефлексию. Данный вариант не является
+//    // рекомендуемым.
+//    @Qualifier("catBean")  // Если при использовании аннотации Autowired подходящих по типу бинов больше одного, то
+//    // выбрасывается исключение. Предотвратить выброс данного исключения можно, конкретно указав, какой бин должен
+//    // быть внедрён. Для этого и существует аннотация Qualifier.
     private Pet pet;
     private String surname;
     private int age;
@@ -15,13 +19,15 @@ public class Person {
         System.out.println("Использовался конструктор без аргументов");
     }
 
-//    @Autowired  // Автоматическое внедрение зависимостей благодаря аннотации Autowired. Начиная с версии Spring 7, при
-//    // наличии у класса одного конструктора, можно не указывать аннотацию Autowired, ибо Spring будет внедрять
-//    // зависимость на одном этом конструкторе.
-//    public Person(Pet pet) {
-//        System.out.println("Использовался конструктор с аргументом");
-//        this.pet = pet;
-//    }
+    @Autowired  // Автоматическое внедрение зависимостей благодаря аннотации Autowired. Начиная с версии Spring 7, при
+    // наличии у класса одного конструктора, можно не указывать аннотацию Autowired, ибо Spring будет внедрять
+    // зависимость на одном этом конструкторе.
+    public Person(@Qualifier("catBean") Pet pet) {  // Аннотацию Qualifier для конструктора необходимо писать перед
+        // параметром конструктора (в документации указано, что тип элемента не может быть конструктором для данной
+        // аннотации).
+        System.out.println("Использовался конструктор с аргументом");
+        this.pet = pet;
+    }
 
     public String getSurname() {
         return surname;
@@ -43,6 +49,7 @@ public class Person {
 
 //    @Autowired  // Необязательно эту аннотацию подставлять именно к сеттеру. По факту её можно использовать с любым
 //    // методом.
+//    @Qualifier("dog")
     public void setPet(Pet pet) {
         System.out.println("Использовался сеттер животного");
         this.pet = pet;
