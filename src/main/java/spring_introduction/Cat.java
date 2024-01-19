bd21fb7b4c95189c7d745f2303557e6f3d5a42f4
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-19 14:37:57 +0300

    Explored the creation of Bean through the Component annotation

--- src/main/java/spring_introduction/Cat.java
+++ src/main/java/spring_introduction/Cat.java
@@ -1,5 +1,10 @@
 package spring_introduction;
 
+import org.springframework.stereotype.Component;
+
+@Component  // Если к аннотации @Component не прописать bean id, то бину будет назначен дефолтный id. Дефолтный id
+// получается из имени класса, заменяя его первую заглавную букву на прописную. Однако если хотя бы 2 заглавные буквы в
+// начале идут подряд, то id будет равен названию класса точь-в-точь.
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Создание кошки через конструктор");
