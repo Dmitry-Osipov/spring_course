e8b181e44b88656e8036cf4176c9665bd63e45bc
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-20 18:12:17 +0300

    Learned pointcut expressions with wildcards or not

--- src/main/java/aop/aspects/LoggingAspect.java
+++ src/main/java/aop/aspects/LoggingAspect.java
@@ -1,5 +1,6 @@
 package aop.aspects;
 
+import aop.AbstractLibrary;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -15,9 +16,16 @@ public class LoggingAspect {
     // * After/After finally - выполняется после окончания метода с основной логикой;
     // * Around - выполняется до и после метода с основной логикой.
 
-    @Before("execution(public void getBook())")  // Это pointcut - выражение, когда должен быть применён Advice.
+    @Before("execution(public void get*())")  // Это pointcut - выражение, когда должен быть применён Advice.
+    // Можно указать для уточнения пакет, класс, исключение. Также можно указать wildcard. Данное выражение отработает
+    // на все public void get*()(* - это любое продолжение) любого класса данного пакета.
     public void beforeGetBookAdvice() {  // Advice - это метод, который находится в Aspect-классе. Он определяет, что
         // должно произойти при вызове метода getBook.
         System.out.println("beforeGetBookAdvice: попытка получить книгу");
+    }
+
+    @Before("execution(public * returnBook())")  // Данное выражение сработает на любой тип возвращаемых данных.
+    public void beforeReturnBook() {
+        System.out.println("beforeReturnBook: попытка вернуть книгу");
     }
 }
