8578df9947ecb255f9735e5da4fcefe499f10263
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-20 18:39:23 +0300

    Learned pointcut expressions with wildcards

--- src/main/java/aop/Test1.java
+++ src/main/java/aop/Test1.java
@@ -7,12 +7,13 @@ public class Test1 {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
-        uniLibrary.getBook();
-        uniLibrary.returnBook();
+        Book book = context.getBean("book", Book.class);
+        uniLibrary.getBook(book);
+//        uniLibrary.returnBook();
         uniLibrary.getMagazine();
-
-        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
-        schoolLibrary.getBook();
+//
+//        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
+//        schoolLibrary.getBook();
 
         context.close();
     }
