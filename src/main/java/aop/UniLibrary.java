c20f5cc403357c617c28f290ed1eed10257a22ec
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-21 19:22:23 +0300

    Studied combining Pointcuts

--- src/main/java/aop/UniLibrary.java
+++ src/main/java/aop/UniLibrary.java
@@ -15,4 +15,16 @@ public class UniLibrary extends AbstractLibrary {
     public void getMagazine() {
         System.out.println("Мы берём журнал из UniLibrary\n");
     }
+
+    public void returnMagazine() {
+        System.out.println("Мы возвращаем журнал в UniLibrary\n");
+    }
+
+    public void addBook() {
+        System.out.println("Мы добавляем книгу в UniLibrary\n");
+    }
+
+    public void addMagazine() {
+        System.out.println("Мы добавляем журнал в UniLibrary\n");
+    }
 }
--- src/main/java/aop/aspects/LoggingAndSecurityAspect.java
+++ src/main/java/aop/aspects/LoggingAndSecurityAspect.java
@@ -23,22 +23,44 @@ public class LoggingAndSecurityAspect {
     // пример: public void get*(String);
     // * параметры и тип параметров - .. (обязательный параметр) - пример: public Integer calculateSum(..);
     // * исключение - просто не указать исключение (необязательный параметр).
-    @Pointcut("execution(* get*())")  // При смене pointcut выражения его требуется поменять лишь здесь, ибо всё, что
-    // ниже прописано с именем данного метода - лишь ссылки.
-    private void allGetMethods() {}  // Объявили pointcut. Плюсы такого подхода: возможность использования созданного
+
+    @Pointcut("execution(* aop.UniLibrary.get*())")  // При смене pointcut выражения его требуется поменять лишь здесь,
+    // ибо всё, что ниже прописано с именем данного метода - лишь ссылки.
+    private void allGetMethodsFromUniLibrary() {}  // Объявили pointcut. Плюсы такого подхода: возможность использования созданного
     // pointcut для множества advice; возможность быстрого изменения pointcut выражения для множества advice;
     // возможность комбинирования pointcut.
+    @Pointcut("execution(* aop.UniLibrary.return*())")
+    private void allReturnMethodsFromUniLibrary() {}
+    @Pointcut("allReturnMethodsFromUniLibrary() || allGetMethodsFromUniLibrary()")  // Комбинируем pointcut.
+    private void allGetOrReturnMethodsFromUniLibrary() {}
+    @Pointcut("execution(* aop.UniLibrary.*(..))")
+    private void allMethodsFromUniLibrary() {}
+    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
+    private void returnMagazineFromUniLibrary() {}
+    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")  // Создали pointcut, который включает в
+    // себя все методы, кроме returnMagazine.
+    private void allMethodsExceptReturnMagazineFromUniLibrary() {}
 
-    @Before("allGetMethods()")  // Это pointcut - выражение, когда должен быть применён Advice.
+    @Before("allGetMethodsFromUniLibrary()")  // Это pointcut - выражение, когда должен быть применён Advice.
     // Если мы используем в качестве параметра кастомный класс, то требуется прописать его название полностью
     // (пакет + название класса).
     public void beforeGetLoggingAdvice() {  // Advice - это метод, который находится в Aspect-классе. Он определяет, что
         // должно произойти при вызове метода getBook.
-        System.out.println("beforeGetBookAdvice: попытка получить книгу/журнал");
+        System.out.println("beforeGetLoggingAdvice: writing Log #1");
     }
 
-    @Before("allGetMethods()")
-    public void beforeGetSecurityAdvice() {
-        System.out.println("beforeGetSecurityAdvice: проверка прав на получение книги/журнала");
+    @Before("allReturnMethodsFromUniLibrary()")
+    public void beforeReturnLoggingAdvice() {
+        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
+    }
+
+    @Before("allGetOrReturnMethodsFromUniLibrary()")
+    public void beforeGetOrReturnLoggingAdvice() {
+        System.out.println("beforeGetOrReturnLoggingAdvice: writing Log #3");
+    }
+
+    @Before("allMethodsExceptReturnMagazineFromUniLibrary()")
+    public void beforeAllMethodsExceptReturnMagazineAdvice() {
+        System.out.println("beforeAllMethodsExceptReturnMagazine: writing Log#4");
     }
 }
