ade0c45cde7f40a63aa7b0167250761f92b486ee
Dmitry Osipov <dimaosipov00@gmail.com>
2024-01-30 14:25:57 +0300

    Explored the workings of AfterReturning. Learned how the AfterReturning annotation can be used to change the values of the returned data

---
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -0,0 +1,36 @@
+package aop.aspects;
+
+import aop.Student;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Aspect
+public class UniversityLoggingAspect {
+    @Before("execution(* getStudents())")
+    public void beforeGetStudentsLoggingAdvice() {
+        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение списка студентов перед " +
+                "методом getStudents");
+    }
+
+    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")  // С помощью AfterReturning можно
+    // менять возвращаемый результат метода.
+    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {  // Название returning должно совпадать
+        // с именем параметра.
+        Student firstStudent = students.getFirst();
+        String nameSurname = firstStudent.getNameSurname();
+        nameSurname = "Mr. " + nameSurname;
+        firstStudent.setNameSurname(nameSurname);
+
+        double avgGrade = firstStudent.getAvgGrade();
+        avgGrade = avgGrade + 1;
+        firstStudent.setAvgGrade(avgGrade);
+
+        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение списка студентов после " +
+                "работы метода getStudents");
+    }
+}
